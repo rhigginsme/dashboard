c62fb60a4ccb4708293796f1f2219c2d6481aa8a
Denise Higgins <denisehiggins@Denises-iMac.local>
2017-10-08 14:46:46 -0400

    fix jquery namespacing and versions

--- home-dash.php
+++ home-dash.php
@@ -15,11 +15,11 @@
   <link rel="stylesheet" href="css/style.css">
   <base href="/">
   <title>Home Dashboard</title>
-  <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
   <script>
-  $(document).ready(function($) {
+  $(document).ready(function() {
     $.simpleWeather({
       location: 'Auburn, ME',
       woeid: '',
@@ -98,7 +98,7 @@
     $("#forecast-open-4").click(function() {
       $("#forecast-4").dialog( "open" );
     });
-  })(jQuery);
+  });
   </script>
   <script>
   function startTime() {
