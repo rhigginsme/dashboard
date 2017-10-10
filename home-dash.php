cd714de2deb65e8491d4df0a8cdf32f8539bfc2a
Denise Higgins <denisehiggins@Denises-iMac.local>
2017-10-10 15:42:31 -0400

    Add new styles

--- home-dash.php
+++ home-dash.php
@@ -120,7 +120,7 @@
     }).css("font-size", "20px");
 
     $("#forecast-open-1").click(function() {
-      $("#forecast-2").dialog({title: "mytitle"}).dialog("open");
+      $("#forecast-2").dialog({title: this.title}).dialog("open");
     });
     $("#forecast-open-2").click(function() {
       $("#forecast-2").dialog("open");
