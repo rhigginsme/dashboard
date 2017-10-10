ff882531f79d76d0614a35e2356e3f15a7821e0c
Denise Higgins <denisehiggins@Denises-iMac.local>
2017-10-10 15:55:03 -0400

    Added cleanup to js and css

--- home-dash.php
+++ home-dash.php
@@ -19,7 +19,7 @@
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
-  <script src="js/home-dash.js"></script>
+  <script src="dashboard/js/home-dash.js"></script>
 </head>
 
 <body onload="startTime()" style="background-color: #000; color: #FFF; width: 100%; height: 100%; margin: 0px; padding: 0px; overflow: hidden;" scroll="no">
