1ea385c7034cdcb5916cf8d0893bfa67f141e5e6
Denise Higgins <denisehiggins@Denises-iMac.local>
2017-10-11 18:50:05 -0400

    Add fix for incorrect dates

--- js/home-dash.js
+++ js/home-dash.js
@@ -25,9 +25,9 @@ $(document).ready(function() {
       $("#forecast-4 > .date").html(weather.forecast[4].text);
 
       $("#forecast-1 > .temps").html('Low: '+weather.forecast[1].low+' / High: '+weather.forecast[1].high);
-      $("#forecast-2 > .temps").html('Low: '+weather.forecast[2].low+'/ High: '+weather.forecast[2].high);
-      $("#forecast-3 > .temps").html('Low: '+weather.forecast[3].low+' / High: '+weather.forecast[3].high);
-      $("#forecast-4 > .temps").html('Low: '+weather.forecast[4].low+' / High: '+weather.forecast[4].high);
+    //  $("#forecast-2 > .temps").html('Low: '+weather.forecast[2].low+'/ High: '+weather.forecast[2].high);
+    //  $("#forecast-3 > .temps").html('Low: '+weather.forecast[3].low+' / High: '+weather.forecast[3].high);
+    //  $("#forecast-4 > .temps").html('Low: '+weather.forecast[4].low+' / High: '+weather.forecast[4].high);
 
       $("#forecast-1 > .forecast").html('<img src="'+weather.forecast[1].image+'"/>');
       $("#forecast-2 > .forecast").html('<img src="'+weather.forecast[2].image+'"/>');
