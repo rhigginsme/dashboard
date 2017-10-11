09bcb0ff968121c4c8dc4656bc764cb737321087
Denise Higgins <denisehiggins@Denises-iMac.local>
2017-10-11 18:47:23 -0400

    Add fix for incorrect dates

--- js/home-dash.js
+++ js/home-dash.js
@@ -20,18 +20,18 @@ $(document).ready(function() {
 
       $("#forecast-1 > .date").html(weather.forecast[1].text);
       $("#forecast-2 > .date").html(weather.forecast[2].text);
-      $("#forecast-3 > .date").html(weather.forecast[3].text);
-      $("#forecast-4 > .date").html(weather.forecast[4].text);
+      //$("#forecast-3 > .date").html(weather.forecast[3].text);
+      //$("#forecast-4 > .date").html(weather.forecast[4].text);
 
       $("#forecast-1 > .temps").html('Low: '+weather.forecast[1].low+' / High: '+weather.forecast[1].high);
       $("#forecast-2 > .temps").html('Low: '+weather.forecast[2].low+'/ High: '+weather.forecast[2].high);
-      $("#forecast-3 > .temps").html('Low: '+weather.forecast[3].low+' / High: '+weather.forecast[3].high);
-      $("#forecast-4 > .temps").html('Low: '+weather.forecast[4].low+' / High: '+weather.forecast[4].high);
+      //$("#forecast-3 > .temps").html('Low: '+weather.forecast[3].low+' / High: '+weather.forecast[3].high);
+      //$("#forecast-4 > .temps").html('Low: '+weather.forecast[4].low+' / High: '+weather.forecast[4].high);
 
       $("#forecast-1 > .forecast").html('<img src="'+weather.forecast[1].image+'"/>');
       $("#forecast-2 > .forecast").html('<img src="'+weather.forecast[2].image+'"/>');
-      $("#forecast-3 > .forecast").html('<img src="'+weather.forecast[3].image+'"/>');
-      $("#forecast-4 > .forecast").html('<img src="'+weather.forecast[4].image+'"/>');
+      //$("#forecast-3 > .forecast").html('<img src="'+weather.forecast[3].image+'"/>');
+      //$("#forecast-4 > .forecast").html('<img src="'+weather.forecast[4].image+'"/>');
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
